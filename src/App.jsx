8e6e45d56e6a6a28836b5c9b5d8c298333e63ae7
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2024-10-27 07:25:23 -0500

    Project: Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navber";
 import Hero from "./components/Hero.jsx";
+import Card from "./components/Card.jsx";
 
 function App() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Card />
     </main>
   );
 }
