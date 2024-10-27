006cf1248c78467c191d86c46f9cd271f0800337
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2024-10-27 07:03:50 -0500

    Project setup: Here

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navber";
+import Hero from "./components/Hero.jsx";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }
